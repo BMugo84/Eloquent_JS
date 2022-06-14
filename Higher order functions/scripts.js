e8bdfff0083779ea5d07e3d20bffed36636a4ca9
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-14 23:31:50 +0300

    function that sums a collection of numbers

--- Higher order functions/scripts.js
+++ Higher order functions/scripts.js
@@ -1144,3 +1144,13 @@ var SCRIPTS = [
     }
     let rtlScripts = filter(SCRIPTS, script =>script.direction == "rtl")
     console.log(JSON.stringify(map(rtlScripts,s =>s.name)))
+  
+    function reduce(array, combine, start) {
+        let current = start
+        for(let element of array){
+            current = combine(current, element)
+
+        }
+        return current
+    }
+    console.log(reduce([1,2,3,4],(a,b) => a + b, 1))
