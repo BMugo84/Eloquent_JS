d7d5d78fbb6aaf4245190d032c241bc2f3e09b05
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-14 12:01:11 +0300

    functions that change other functions

--- Higher order functions/hof.js
+++ Higher order functions/hof.js
@@ -3,3 +3,13 @@ function greaterthan(n) {
 }
 let greaterthan10 = greaterthan(10)
 console.log(greaterthan10(11))
+
+function noisy(f){
+    return (...args) => {
+        console.log("calling with", args)
+        let result = f(...args)
+        console.log("called with", args, ", returned", result)
+        return result
+    }
+}
+noisy(Math.min)(3,2,1)
