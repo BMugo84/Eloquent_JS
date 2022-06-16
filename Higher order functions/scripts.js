f44a89c4ece38d6818ef9c90cbcfddb56bac9511
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-16 12:01:00 +0300

    gets total no of chars in argument using reduce and transform counting entries into readable strings with map and combine them with join

--- Higher order functions/scripts.js
+++ Higher order functions/scripts.js
@@ -1243,13 +1243,16 @@ function textScripts(text) {
     return script? script.name : "none"
   }).filter(({name})=> name != "none")
 
-  // let total = scripts.reduce((n, {count})=> n + count, 0)
-  // if (total == 0) return "no scripts found"
+  let total = scripts.reduce((n, {count})=> n + count, 0)
+  if (total == 0) return "no scripts found"
 
-  // return scripts.map(({name, count}) => {
-  //   return `${Math.round(count * 100 / total)}% ${name}`
-  // }).join(", ")
-  return scripts
+  return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`
+  }).join(", ")
+ //   return scripts
 }
 console.log(JSON.stringify(textScripts('英国的狗说woof, 俄罗斯的狗说"тяв"')))
+
+
+
 
