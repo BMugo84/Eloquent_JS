fa35136c12ce70f849fe677e1052685c49b38b9e
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-19 21:54:57 +0300

    every function using some function

--- Higher order functions/Everything.js
+++ Higher order functions/Everything.js
@@ -4,17 +4,33 @@
     
 // }
 
-function every(array, test){
-    for (const element of array) {
-        if (!test(element)){
-            return false
-        }
-    }
-    return true
-    
-}
-console.log(every([1,2,3,7,4,5], l=> l<6))
-
-// function every(array, predicatefn) {
+// function every(array, test){
+//     for (const element of array) {
+//         if (!test(element)){
+//             return false
+//         }
+//     }
+//     return true
     
 // }
+// console.log(every([1,2,3,7,4,5], l=> l<6))
+
+function every(array, predicatefn) {
+    if (!array.some(element => !predicatefn(element))) {
+        return `some elements don't satisfy the function`
+    }
+    return `all elements satisfy the function`
+}
+
+console.log(every([1,2,3,4,5], e=> e>1 ))
+
+// let a = 1>5
+// let b = 1 > 10
+// console.log(!( !a || !b ))
+// console.log(a&&b)
+// e => e>5
+
+function every(array,test){
+    return !array.some(element => !test(element))
+}
+console.log(every([1,2,3,4,5], e=> e<6 ))
