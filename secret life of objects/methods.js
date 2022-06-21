1c550b7aa27a460f509fa685397d06c0a1105bcc
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-21 17:09:13 +0300

    this in arrow functions refers to the object outside its scope ie outside its encapsulation boundary

--- secret life of objects/methods.js
+++ secret life of objects/methods.js
@@ -11,3 +11,14 @@ whiteRabbit.speak("I hate the RAIN! Makes my fur all wet and dirty")
 hungryRabbit.speak("All these juicy carrots make me drool!")
 
 speak.call(hungryRabbit, "burp")
+
+function normalize(){
+    console.log( this.coords.map(n => n/ this.length) )
+}
+normalize.call({coords: [0, 2, 3], length: 5 })
+
+// const numbers = [1, -1, 2, 3]
+//  const filtered = numbers.filter(n => n >= 0)
+//  const items = filtered.map(n => '(' + n + ')')
+
+//  console.log(items)
