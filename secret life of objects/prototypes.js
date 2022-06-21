296b1b5881d7fc5e7f83c702931a3978fa1cfffb
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-21 21:40:38 +0300

    prototype testing

--- secret life of objects/prototypes.js
+++ secret life of objects/prototypes.js
@@ -3,6 +3,14 @@ let empty = {}
 console.log(empty.toString)
 console.log(empty.toString())
 
+console.log(Object.getPrototypeOf({}) == Object.prototype)
+console.log(Object.getPrototypeOf(Object.prototype))
+console.log(Object.getPrototypeOf({}))
+
+console.log(Object.getPrototypeOf(Math.max) == Function.prototype)
+console.log(Object.getPrototypeOf([]) == Array.prototype)
+
+
 
 
 
