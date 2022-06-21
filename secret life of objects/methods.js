8c852d4afdea71fcaf50c619ba0f683d2e8bf66f
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-21 15:15:42 +0300

    introducing this binding

--- secret life of objects/methods.js
+++ secret life of objects/methods.js
@@ -1,5 +1,11 @@
-let rabbit = {}
-rabbit.speak = line => {
-    console.log(`The rabbit says '${line}'`)
+function speak(line) {
+    console.log(`The ${this.type} rabbit said '${line}'`)
 }
-rabbit.speak("I'm alive")
+// rabbit.speak("I'm alive")
+// this points to the object that it was called on
+
+let whiteRabbit = {type: "white", speak}
+let hungryRabbit = {type: "hungry", speak}
+ 
+whiteRabbit.speak("I hate the RAIN! Makes my fur all wet and dirty")
+hungryRabbit.speak("All these juicy carrots make me drool!")
