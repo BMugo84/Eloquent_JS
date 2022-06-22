a34a7f8da6b39f42f2e7462b8418bc1a714ce02f
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-22 20:41:04 +0300

    map method to avoid the toString error

--- secret life of objects/maps(noun).js
+++ secret life of objects/maps(noun).js
@@ -1,9 +1,17 @@
-let ages = {
-    Boris: 39,
-    Liang: 22,
-    Julia: 62
-}
-console.log(`Julia is ${ages["Julia"]}`)
-console.log(ages["Julia"])
-console.log("Liang" in ages)
-console.log("toString" in ages)
+// let ages = {
+//     Boris: 39,
+//     Liang: 22,
+//     Julia: 62
+// }
+// console.log(`Julia is ${ages["Julia"]}`)
+// console.log(ages["Julia"])
+// console.log("Liang" in ages)
+// console.log("toString" in ages)
+
+let ages = new Map()
+ages.set("Boris",39)
+ages.set("Liang", 22)
+ages.set("Julia", 62)
+
+console.log(`Julia is ${ages.get("Julia")}`)
+console.log( ages.has("toString"))
