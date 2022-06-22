c5b06e38c87c29b1d7f0b6cf959c1481e328c04b
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-22 20:31:34 +0300

    basic mapping

---
+++ secret life of objects/maps(noun).js
@@ -0,0 +1,9 @@
+let ages = {
+    Boris: 39,
+    Liang: 22,
+    Julia: 62
+}
+console.log(`Julia is ${ages["Julia"]}`)
+console.log(ages["Julia"])
+console.log("Liang" in ages)
+console.log("toString" in ages)
