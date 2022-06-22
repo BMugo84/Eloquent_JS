ff8edca6f71686e1906afad795fc4496e848df86
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-22 23:10:59 +0300

    symbols. didnt understand them much

--- secret life of objects/polymrphism.js
+++ secret life of objects/polymrphism.js
@@ -20,3 +20,15 @@ Rabbit.prototype.toString = function(){
     return `a ${this.type} rabbit`
 }
 console.log(String(blackRabbit))
+
+let sym = Symbol("name")
+console.log(sym == Symbol("name"))
+Rabbit.prototype[sym] = 55
+console.log(blackRabbit[sym])
+
+const toStringSymbol = Symbol("toString")
+Array.prototype[toStringSymbol] = function(){
+    return ` ${this.length} cm of blue yarn`
+}
+console.log([1, 2].toString())
+console.log([1, 2][toStringSymbol]())
