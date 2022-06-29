c4f8d9d6e0f0e473bdbe24bc4aa2899b5402c658
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-29 21:23:17 +0300

    scope of var

---
+++ introduction and basics/VarLetConst.js
@@ -0,0 +1,6 @@
+// difference between var, let const
+ var greeter = "say hi"      //globally scoped
+function newfunction() {
+    var hello = hello       //function(locally) scoped . can only be accesed byb the function    
+}
+console.log(hello)
