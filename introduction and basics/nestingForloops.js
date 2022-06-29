310667faffa204bd56ef4625b58d9fa572972b57
BMugo84 <mugo.benson.kbm@gmail.com>
2022-06-30 01:13:16 +0300

    Nesting For Loops in JavaScript
    In the editor on the right, multiplyAll() is defined as follows:

---
+++ introduction and basics/nestingForloops.js
@@ -0,0 +1,11 @@
+// In this problem you have to complete the multiplyAll() function, and takes a
+//  multi-dimensional array as an argument. Remember that a multi-dimensional
+//   array, sometimes called a 2D array, is just an array of arrays, for example,
+//    [[1,2], [3,4], [5,6]].
+
+function multiplyAll(arr) {
+    // only change code below this line
+
+    // only change code above this line
+}
+multiplyAll([[1,2],[3,4],[5,6,7]])
