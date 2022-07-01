737501dfa86bc67fa8027b4a9234152be964f32a
BMugo84 <mugo.benson.kbm@gmail.com>
2022-07-01 20:42:24 +0300

    class vector
    Vector takes in two parameters x and y and sets their instance properties

--- secret life of objects/vectorType.js
+++ secret life of objects/vectorType.js
@@ -1,57 +1,3 @@
-class Vector {
-    constructor(width,height, element = (x,y) => undefined) {
-         this.width = width; 
-         this.height = height;
-         this.content = [];
-
-         for (let y = 0; y < 3 ; y++) {
-            for (let x = 0; x < 2; x++) {
-                this.content[y * width + x] = element(x,y);
-            }   
-         }
-     }
-
-    get(x,y) {
-        return this.content[y * this.width + x];
-    }
-    set(x, y, value) {
-        this.content[y * this.width + x] = value;
-   } 
-}
-
-class VectorIterator{
-    constructor(vector){
-        this.x = 0;
-        this.y = 0;
-        this.vector = vector
-    }
-
-    next(){
-        if (this.y == this.vector.height) return {done: true }
-
-        let value = {
-            x: this.x,
-            y: this.y,
-            value: this.vector.get(this.x, this.y)
-        }
-        this.x++
-
-        if (this.x == this.vector.width) {
-            this.x = 0
-            this.y++
-        }
-        return {value, done: false}
-    }
-}
-
-Vector.prototype[Symbol.iterator] = function() {
-    return new VectorIterator(this)
-}
-
-let vector = new Vector(1,2, (x,y) => `value ${x}, ${y}`)
-for (let {x, y, value}of vector){
-    console.log(` x is ${x},  y is ${y},  ${value}`)
-}
 
 //the correct code according to the book
 
@@ -65,3 +11,11 @@ for (let {x, y, value}of vector){
 // the square of the y-coordinate.
 // Thus, (x.sqrd + y.sqrd)sqrt is the number you want, and
 // Math.sqrt is the way you compute a square root in JavaScript.
+
+class Vector {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
