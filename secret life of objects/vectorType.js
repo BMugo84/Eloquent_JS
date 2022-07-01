c7b424aad65ebe1d5c66e8bb1034085c25aca390
BMugo84 <mugo.benson.kbm@gmail.com>
2022-07-01 16:52:08 +0300

    procedure

--- secret life of objects/vectorType.js
+++ secret life of objects/vectorType.js
@@ -52,3 +52,16 @@ let vector = new Vector(1,2, (x,y) => `value ${x}, ${y}`)
 for (let {x, y, value}of vector){
     console.log(` x is ${x},  y is ${y},  ${value}`)
 }
+
+//the correct code according to the book
+
+// Look back to the Rabbit class example if you’re unsure how class declarations
+// look.
+// Adding a getter property to the constructor can be done by putting the
+// word get before the method name.
+// To compute the distance from (0, 0) to
+// (x, y), you can use the Pythagorean theorem, which says that the square of
+// the distance we are looking for is equal to the square of the x-coordinate plus
+// the square of the y-coordinate.
+// Thus, (x.sqrd + y.sqrd)sqrt is the number you want, and
+// Math.sqrt is the way you compute a square root in JavaScript.
